711ca0f653f4847472fba77b503274ba2c4be789
bayubagusbagaswara <bagaszwara12@gmail.com>
2021-05-25 20:49:26 +0700

    membuat entity

---
+++ entity/Todolist.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Entity {
+
+    class TodoList
+    {
+        // buat properti
+        // cuma butuh data todo nya
+        private string $todo;
+
+        // constructor, tapi optional parameternya
+        public function __construct(string $todo = "")
+        {
+            $this->todo = $todo;
+        }
+        // getter dan setter
+        public function getTodo(): string
+        {
+            return $this->todo;
+        }
+
+        public function setTodo(string $todo): void
+        {
+            $this->todo = $todo;
+        }
+    }
+}
