6d3bab06faa47339b1bd8db7e9bf0c729a7d5953
bayubagusbagaswara <bagaszwara12@gmail.com>
2021-05-25 21:02:36 +0700

    membuat view todolist

---
+++ view/TodolistView.php
@@ -0,0 +1,17 @@
+<?php
+
+// implementasi view nya
+namespace View {
+    class TodolistView
+    {
+        function showTodolist(): void
+        {
+        }
+        function addTodolist(): void
+        {
+        }
+        function removeTodolist(): void
+        {
+        }
+    }
+}
