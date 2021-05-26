35526c27da22f3ab6585b618ec609037038c510d
bayubagusbagaswara <bagaszwara12@gmail.com>
2021-05-26 09:18:16 +0700

    implementasi view untuk menampilkan todolist

--- view/TodolistView.php
+++ view/TodolistView.php
@@ -2,10 +2,42 @@
 
 // implementasi view nya
 namespace View {
+
+    use Service\TodolistService;
+    use Helper\InputHelper;
+
     class TodolistView
     {
+        // manggil service nya
+        private TodolistService $todolistService;
+        public function __construct(TodolistService $todolistService)
+        {
+            $this->todolistService = $todolistService;
+        }
+
         function showTodolist(): void
         {
+            // view menampilkan todolist
+            while (true) {
+                $this->todolistService->showTodolist();
+
+                echo "MENU" . PHP_EOL;
+                echo "1. Tambah Todo" . PHP_EOL;
+                echo "2. Hapus Todo" . PHP_EOL;
+                echo "3. Keluar" . PHP_EOL;
+
+                $pilihan = InputHelper::input("Pilih");
+                if ($pilihan == "1") {
+                    $this->addTodolist();
+                } else if ($pilihan == "2") {
+                    $this->removeTodolist();
+                } else if ($pilihan == "x") {
+                    break;
+                } else {
+                    echo "Pilihan tidak dimengerti" . PHP_EOL;
+                }
+            }
+            echo "Sampai jumpa lagi" . PHP_EOL;
         }
         function addTodolist(): void
         {
